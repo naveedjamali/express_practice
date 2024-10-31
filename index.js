40f55cf20d2dae783a095c92463da5f9277b484c
Naveed Jamali <naveedjamali@outlook.com>
2024-10-31 19:00:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.static(path.join(__dirname,"static")))
+
+app.get('/', require(path.join(__dirname,'/routes/blog')))
+
+app.listen(port,()=>{
+    console.log('Listening at port:'+port)
+})
+
+
+
